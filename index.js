51f372efe46dc6fde0abc5f0196ed656c51ae118
Gvila92 <145710383+Gvila92@users.noreply.github.com>
2023-10-18 14:48:11 -0400

    Update index.js
    added search functionality

--- index.js
+++ index.js
@@ -1,121 +1,144 @@
 const content = document.getElementById("content");
-let data = {};
+        let data = {};
 
-const artistData = [
-    { id: "6l3HvQ5sa6mXTsMTB19rO5", name: "J-Cole" },
-    { id: "1uNFoZAHBGtllmzznpCI3s", name: "Justin Bieber" },
-    { id: "790FomKkXshlbRYZFtlgla", name: "Karol G" },
-    { id: "12Chz98pHFMPJEknJQMWvI", name:"Muse"},
-    {id: "181bsRPaVXVlUKXrxwZfHK", name:"Megan the Stallion"}
-    // Add more artists with their IDs and names
-];
+        const artistData = [
+            { id: "6l3HvQ5sa6mXTsMTB19rO5", name: "J-Cole" },
+            { id: "1uNFoZAHBGtllmzznpCI3s", name: "Justin Bieber" },
+            { id: "790FomKkXshlbRYZFtlgla", name: "Karol G" },
+            { id: "12Chz98pHFMPJEknJQMWvI", name: "Muse" },
+            { id: "181bsRPaVXVlUKXrxwZfHK", name: "Megan Thee Stallion" }
+            // Add more artists with their IDs and names
+        ];
 
-const requestToken = async () => {
-    const response = await fetch("https://accounts.spotify.com/api/token", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: "grant_type=client_credentials&client_id=64ffb24515d44051b073917a2bd60326&client_secret=7ba47ffd43fc4c799c3e9a64b02b2456"
-    });
-    data = await response.json();
-    console.log(data);
-};
+        const requestToken = async () => {
+            const response = await fetch("https://accounts.spotify.com/api/token", {
+                method: "POST",
+                headers: { "Content-Type": "application/x-www-form-urlencoded" },
+                body: "grant_type=client_credentials&client_id=64ffb24515d44051b073917a2bd60326&client_secret=7ba47ffd43fc4c799c3e9a64b02b2456"
+            });
+            data = await response.json();
+            console.log(data);
+        };
 
-const renderPage = async () => {
-    await requestToken();
+        const renderPage = async () => {
+            await requestToken();
 
-    artistData.forEach((artist) => {
-        fetchArtist(artist);
-    });
-};
+            artistData.forEach((artist) => {
+                fetchArtist(artist);
+            });
 
-const fetchArtist = async (artist) => {
-    const response = await fetch(`https://api.spotify.com/v1/artists/${artist.id}`, {
-        headers: { "Authorization": `Bearer ${data.access_token}` }
-    });
+            const searchButton = document.getElementById("searchButton");
+            searchButton.addEventListener("click", searchArtist);
+        };
 
-    const artistInfo = await response.json();
-    const artistContainer = document.createElement("div");
-    artistContainer.classList.add("artist-container");
+        const fetchArtist = async (artist) => {
+            const response = await fetch(`https://api.spotify.com/v1/artists/${artist.id}`, {
+                headers: { "Authorization": `Bearer ${data.access_token}` }
+            });
 
-    const artistImage = document.createElement("img");
-    artistImage.src = artistInfo.images[1].url;
+            const artistInfo = await response.json();
+            const artistContainer = document.createElement("div");
+            artistContainer.classList.add("artist-container");
 
-    const artistName = document.createElement("h2");
-    artistName.textContent = artist.name;
+            const artistImage = document.createElement("img");
+            artistImage.src = artistInfo.images[1].url;
 
-    artistContainer.appendChild(artistImage);
-    artistContainer.appendChild(artistName);
-    artistContainer.setAttribute("data-artist", artist.id);
+            const artistName = document.createElement("h2");
+            artistName.textContent = artist.name;
 
-    content.appendChild(artistContainer);
+            artistContainer.appendChild(artistImage);
+            artistContainer.appendChild(artistName);
+            artistContainer.setAttribute("data-artist", artist.id);
 
-    artistImage.addEventListener("click", () => showAlbumDropdown(artist.id));
-};
+            content.appendChild(artistContainer);
 
-const showAlbumDropdown = async (artistId) => {
-    clearAlbumDropdown();
+            artistImage.addEventListener("click", () => showAlbumDropdown(artist.id));
+        };
 
-    const albumDropdown = document.createElement("select");
-    albumDropdown.id = `albumDropdown-${artistId}`;
+        const showAlbumDropdown = async (artistId) => {
+            clearAlbumDropdown();
 
-    const defaultOption = document.createElement("option");
-    defaultOption.textContent = "Select an Album";
-    albumDropdown.appendChild(defaultOption);
+            const albumDropdown = document.createElement("select");
+            albumDropdown.id = `albumDropdown-${artistId}`;
 
-    const response = await fetch(`https://api.spotify.com/v1/artists/${artistId}/albums?market=US`, {
-        headers: { "Authorization": `Bearer ${data.access_token}` }
-    });
+            const defaultOption = document.createElement("option");
+            defaultOption.textContent = "Select an Album";
+            albumDropdown.appendChild(defaultOption);
 
-    const artistAlbums = await response.json();
-    renderAlbumDropdown(artistAlbums.items, artistId, albumDropdown);
-};
+            const response = await fetch(`https://api.spotify.com/v1/artists/${artistId}/albums?market=US`, {
+                headers: { "Authorization": `Bearer ${data.access_token}` }
+            });
 
-const clearAlbumDropdown = () => {
-    const albumDropdowns = content.querySelectorAll("select[id^='albumDropdown-']");
-    albumDropdowns.forEach((dropdown) => dropdown.remove());
-};
+            const artistAlbums = await response.json();
+            renderAlbumDropdown(artistAlbums.items, artistId, albumDropdown);
+        };
 
-const renderAlbumDropdown = (albums, artistId, albumDropdown) => {
-    albums.forEach((album) => {
-        const option = document.createElement("option");
-        option.value = album.id;
-        option.textContent = album.name;
-        albumDropdown.appendChild(option);
-    });
+        const clearAlbumDropdown = () => {
+            const albumDropdowns = content.querySelectorAll("select[id^='albumDropdown-']");
+            albumDropdowns.forEach((dropdown) => dropdown.remove());
+        };
 
-    albumDropdown.addEventListener("change", async (event) => {
-        const selectedAlbumId = event.target.value;
-        await fetchAlbumTracks(selectedAlbumId);
-    });
+        const renderAlbumDropdown = (albums, artistId, albumDropdown) => {
+            albums.forEach((album) => {
+                const option = document.createElement("option");
+                option.value = album.id;
+                option.textContent = album.name;
+                albumDropdown.appendChild(option);
+            });
 
-    const artistContainer = content.querySelector(`[data-artist="${artistId}"]`);
-    artistContainer.appendChild(albumDropdown);
-};
+            albumDropdown.addEventListener("change", async (event) => {
+                const selectedAlbumId = event.target.value;
+                await fetchAlbumTracks(selectedAlbumId);
+            });
 
-const fetchAlbumTracks = async (albumId) => {
-    const response = await fetch(`https://api.spotify.com/v1/albums/${albumId}/tracks`, {
-        headers: { "Authorization": `Bearer ${data.access_token}` }
-    });
+            const artistContainer = content.querySelector(`[data-artist="${artistId}"]`);
+            artistContainer.appendChild(albumDropdown);
+        };
 
-    const albumTracks = await response.json();
-    renderTrackList(albumTracks.items);
-};
+        const fetchAlbumTracks = async (albumId) => {
+            const response = await fetch(`https://api.spotify.com/v1/albums/${albumId}/tracks`, {
+                headers: { "Authorization": `Bearer ${data.access_token}` }
+            });
 
-const renderTrackList = (tracks) => {
-    const trackList = document.createElement("ul");
-    tracks.forEach((track) => {
-        const listItem = document.createElement("li");
-        listItem.textContent = track.name;
-        trackList.appendChild(listItem);
-    });
+            const albumTracks = await response.json();
+            renderTrackList(albumTracks.items);
+        };
 
-    const existingTrackList = content.querySelector("#trackList");
-    if (existingTrackList) {
-        existingTrackList.remove();
-    }
+        const renderTrackList = (tracks) => {
+            const trackList = document.createElement("ul");
+            tracks.forEach((track) => {
+                const listItem = document.createElement("li");
+                listItem.textContent = track.name;
+                trackList.appendChild(listItem);
+            });
 
-    trackList.id = "trackList";
-    content.appendChild(trackList);
-};
+            const existingTrackList = content.querySelector("#trackList");
+            if (existingTrackList) {
+                existingTrackList.remove();
+            }
 
-renderPage();
+            trackList.id = "trackList";
+            content.appendChild(trackList);
+        };
+
+        const searchArtist = async () => {
+            const searchInput = document.getElementById("searchInput");
+            const searchTerm = searchInput.value;
+
+            if (searchTerm) {
+                const response = await fetch(`https://api.spotify.com/v1/search?q=${searchTerm}&type=artist`, {
+                    headers: { "Authorization": `Bearer ${data.access_token}` }
+                });
+
+                const searchResults = await response.json();
+                if (searchResults.artists && searchResults.artists.items.length > 0) {
+                    const artist = searchResults.artists.items[0];
+                    fetchArtist({ id: artist.id, name: artist.name });
+                    searchInput.value = ''; // Clear the search input field
+                } else {
+                    alert("No artists found for the given search term.");
+                }
+            }
+        };
+
+        renderPage();
