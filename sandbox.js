73ec4b53e794e0612ba8f1d91ed824d89be68e93
Gvila92 <145710383+Gvila92@users.noreply.github.com>
2023-10-18 13:05:46 -0400

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -1,51 +1,103 @@
+const content = document.getElementById("content");
+const artistContainer = document.createElement("div"); // Create a container for the artist info
 
-const artist = async () => {
-    const response = await fetch("http://localhost:3000/artist")
-    const info = await response.json();
-    info.forEach(info => {
-        renderInfo(info)
-    });
-    console.log(artist);
-};
+let data = {};
+let selectedAlbumId = null; // Track the selected album ID
 
-
-const artistIds = ["6l3HvQ5sa6mXTsMTB19rO5"]
+const artistIds = ["6l3HvQ5sa6mXTsMTB19rO5"];
 const requestToken = async () => {
-
     const response = await fetch("https://accounts.spotify.com/api/token", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: "grant_type=client_credentials&client_id=64ffb24515d44051b073917a2bd60326&client_secret=7ba47ffd43fc4c799c3e9a64b02b2456"
     });
-    const data = await response.json()
-    console.log(data)
+    data = await response.json();
+    console.log(data);
+};
 
-    const fetchArtist = async (artistId) => {
-        const response = await fetch(`https://api.spotify.com/v1/artists/${artistId}`, {
-            headers: { "Authorization": `Bearer ${data.access_token}` }
-        })
-        const artistInfo = await response.json()
-        console.log(artistInfo)
-        const artist = {image: artistInfo.images[0].url}
-        renderArtist(artist)
-    }
-    artistIds.forEach((artistId) => fetchArtist(artistId))
-}
+const renderPage = async () => {
+    await requestToken();
 
-const content = document.getElementById("content")
+    artistIds.forEach((artistId) => fetchArtist(artistId));
+    fetchArtistAlbums("6l3HvQ5sa6mXTsMTB19rO5"); // Fetch artist albums
+};
+
+const fetchArtist = async (artistId) => {
+    const response = await fetch(`https://api.spotify.com/v1/artists/${artistId}`, {
+        headers: { "Authorization": `Bearer ${data.access_token}` }
+    });
+
+    const artistInfo = await response.json();
+    const artist = { image: artistInfo.images[1].url };
+    renderArtist(artist);
+};
+
+const fetchArtistAlbums = async (artistId) => {
+    const response = await fetch(`https://api.spotify.com/v1/artists/${artistId}/albums?market=US`, {
+        headers: { "Authorization": `Bearer ${data.access_token}` }
+    });
+
+    const artistAlbums = await response.json();
+    renderAlbumDropdown(artistAlbums.items); // Populate the album dropdown
+};
+
+const fetchAlbumTracks = async (albumId) => {
+    const response = await fetch(`https://api.spotify.com/v1/albums/${albumId}/tracks`, {
+        headers: { "Authorization": `Bearer ${data.access_token}` }
+    });
+
+    const albumTracks = await response.json();
+    renderTrackList(albumTracks.items); // Display the tracks associated with the album
+};
 
 const renderArtist = (artist) => {
-    const image = document.createElement("img")
-    image.src = artist.image
-    console.log(image)
-    content.append(image)
-}
+    const artistImage = document.createElement("img");
+    artistImage.src = artist.image;
+    artistContainer.appendChild(artistImage);
 
-const requestTimer = setTimeout(() => { requestToken(), 3600000 })
-// console.log(requestTimer)
+    content.appendChild(artistContainer); // Add the container to the content
+};
 
-//first get all the info of artist
-//image, name ,Genre
-//add other 4 artist ids to the array
-//write the function that will fetch the albums (event listeners)
-//click on album and it will fetch the songs on the album
+const renderAlbumDropdown = (albums) => {
+    const albumDropdown = document.createElement("select"); // Create a select element for the album dropdown
+    albumDropdown.id = "albumDropdown"; // Set an id for the dropdown
+
+    const albumOption = document.createElement("option"); // Create an option element for the default message
+    albumOption.textContent = "Select an Album";
+    albumDropdown.appendChild(albumOption);
+
+    albums.forEach((album) => {
+        const option = document.createElement("option");
+        option.value = album.id;
+        option.textContent = album.name;
+        albumDropdown.appendChild(option);
+    });
+
+    albumDropdown.addEventListener("change", (event) => {
+        selectedAlbumId = event.target.value;
+        fetchAlbumTracks(selectedAlbumId); // Fetch and display tracks when an album is selected
+    });
+
+    artistContainer.appendChild(albumDropdown); // Add the album dropdown to the container
+};
+
+const renderTrackList = (tracks) => {
+    const trackList = document.createElement("ul");
+
+    tracks.forEach((track) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = track.name;
+        trackList.appendChild(listItem);
+    });
+
+    // Clear the previous track list, if any
+    const existingTrackList = content.querySelector("#trackList");
+    if (existingTrackList) {
+        content.removeChild(existingTrackList);
+    }
+
+    trackList.id = "trackList";
+    content.appendChild(trackList); // Add the new track list to the content
+};
+
+renderPage();
